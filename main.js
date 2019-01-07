e6def6e27ae2c301fb57880375ed239a936f7d05
AHMEDABOAMRA <45484644+AHMEDABOAMRA@users.noreply.github.com>
2019-01-07 14:45:11 +0100

    اا

--- main.js
+++ main.js
@@ -35,3 +35,58 @@ $(".kl").click(function(){
 		});
 });
 
+$(document).ready(function(){
+  
+
+    $(".navbar-toggle").click(function(){
+      $(".x").slideToggle(2000);
+  
+  
+  });
+   $(".cy").click(function(){
+      $(".b").slideToggle(1000);
+  
+      $(".cy").css("border-bottom","0px");
+  
+  });
+     
+  $(".xv").click(function(){
+      $(".c").slideToggle(1000);
+  
+      $(".xv").css("border-bottom","0px");
+  
+  });
+  $(".kl").click(function(){
+      $(".d").slideToggle(1000);
+  
+      $(".kl").css("border-bottom","0px");
+  
+  });
+  
+     
+  });
+  
+  $(function() {$('body').on('click', 'a.scrollable', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
+    event.preventDefault();
+    });
+  });
+  
+  // Closes the Responsive Menu on Menu Item Click
+  $('.navbar-collapse ul li a').click(function() {
+    $(".navbar-collapse").collapse('hide');});
+  
+  // Hides the scroll up button at top.
+  $(window).scroll(function() {
+  
+      if ($(this).scrollTop()>0)
+       {
+          $('.scroll-top').fadeIn();
+       }
+      else
+       {
+        $('.scroll-top').fadeOut();
+       }
+   });
+      
